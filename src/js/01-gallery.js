57a2c8191fec4ad9ab89f68f08f7e38285e7b848
Dmytrii Vitkovskyi <dimavitkovskiy2605@gmail.com>
2023-02-02 16:19:11 +0200

    ready-HW-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallaryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+gallaryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+gallaryContainer.addEventListener('click', onImgClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOKAL_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(LOKAL_KEY, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+let saveTime = localStorage.getItem(LOKAL_KEY);
+if (saveTime) {
+  player.setCurrentTime(saveTime);
+}
